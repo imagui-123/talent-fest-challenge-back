66c2415e7a1ea80a22799cf11dedf12efa3658a6
unknown <iaguiarg@hotmail.com>
2019-08-04 14:32:42 -0500

    Agregue validaciones de autorización

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const {getInventories, createInventory} = require('../controllers/inventory');
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 const {createInventoryValidator} = require('../validator');
 
 const router = express.Router();
 
 router.get("/", getInventories);
-router.post("/inventory", createInventoryValidator, createInventory);
+router.post("/inventory", requireSignin, createInventoryValidator, createInventory);
+
+
+//any route containing :userId, our app will first execute userById()
+router.param('userId', userById);
 
 module.exports = router;
 
