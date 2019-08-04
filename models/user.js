732e87f9269cd38b4b81218b39f05a19a955dd50
unknown <iaguiarg@hotmail.com>
2019-08-04 11:45:27 -0500

    Agregue user schema

---
+++ models/user.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+// const {ObjectId} = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    lastname:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    email:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    location:{
+        type: String
+    },
+    hased_password:{
+        type:String,
+        required: true
+    },
+    salt: String, //will be along randomly generated complicated string like so guess.
+    created:{
+        type: Date,
+        default: Date.now
+    },
+    updated: Date,
+});
+
+
+/**
+ * Virtual fields are additional fields for a given model.
+ * Their values can be set manually or automatically with defined functionality.
+ * Keep in mind: virtual properties (password) don´t get presisted in the database.
+ * They only exist logically and are not written to the document´s collection.
+ */
+
+//virtual field
+userSchema.virtual('password')
+.set(function(password){
+    //create temporary variable called _password
+    this._password= password
+    //generate a timestamp
+    this.salt= uuidv1()
+    //encryptPassword()
+    this.hased_password = this.encryptPassword(password)
+})
+.get(function() {
+    return this._password
+})
+
+//methods
+userSchema.methods={
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) == this.hased_password
+    },
+
+    encryptPassword: function(password){
+        if(!password) return "";
+        try{
+            return crypto
+            .createHmac('sha1', this.salt)
+            .update(password)
+            .digest('hex');
+        } catch(err) {
+            return "";
+        }
+    } 
+};
+
+module.exports = mongoose.model('User', userSchema);
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const inventoryController = require('../controllers/inventory');
+const {getInventories, createInventory} = require('../controllers/inventory');
 const validator = require('../validator');
 
 const router = express.Router();
 
-router.get("/", inventoryController.getInventories);
-router.post("/inventory", validator.createInventoryValidator, inventoryController.createInventory);
+router.get("/", getInventories);
+router.post("/inventory", validator.createInventoryValidator, createInventory);
 
 module.exports = router;
 
