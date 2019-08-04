ebf95a21a5c38370b715bee7589ee409d836af7b
unknown <iaguiarg@hotmail.com>
2019-08-04 13:08:06 -0500

    Agregue validaciones para signup

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {getInventories, createInventory} = require('../controllers/inventory');
-const validator = require('../validator');
+const {createInventoryValidator} = require('../validator');
 
 const router = express.Router();
 
 router.get("/", getInventories);
-router.post("/inventory", validator.createInventoryValidator, createInventory);
+router.post("/inventory", createInventoryValidator, createInventory);
 
 module.exports = router;
 
